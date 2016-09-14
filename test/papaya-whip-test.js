454f27a7b472ba29aef54efdf1e4b65d91f006ef
KINAN WHYTE <kinanwhyte@gmail.com>
2016-09-14 11:31:25 -0600

    sets up basic structure for files and respective tests for ball, paddle, blocks, and world

---
+++ test/papaya-whip-test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const Jungle = require('../lib/jungle.js');
+
+describe('Block', function {
+
+  it('should be a function', function () {
+    assert.isFunction(Block);
+  });
+
+  it('should instantiate a block', function () {
+    var block = new Block;
+    assert.isObject(block);
+  });
+
+
+})
